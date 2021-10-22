6fc9e0edba598f9363f0da57ce019fc4b25dce8d
toddlerya <toddlerya@gmail.com>
2021-10-22 17:29:26 +0800

    修改容器网段

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 const GlobalConfig = {
-    pypiServerHost: "192.168.41.3",
+    pypiServerHost: "192.199.41.3",
     pypiServerPort: "8080",
     pypiClientWin: "http://172.16.111.6/X2590/pypiClientTool/uploads/4799db10a5d315edbceddcde58b411be/pypiClientTool.exe",
     pypiClientLinux: "http://172.16.111.6/X2590/pypiClientTool/uploads/527b9bf35fa4f23aca292dba346479ce/pypiClientTool"
