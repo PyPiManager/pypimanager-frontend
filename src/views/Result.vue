cd86b6ec9467145a4005975d70ae9d6e10549177
toddlerya <toddlerya@gmail.com>
2021-10-27 17:26:57 +0800

    优化交互

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -12,30 +12,29 @@
       <p style="font-size: 1rem">共查询到{{ this.tableData.length }}个包</p>
     </el-col>
     <el-col :span="14" :push="5">
-      <div v-if="this.queryStatus">
-        <el-table
-          v-loading="loading"
-          element-loading-text="拼命加载中"
-          :data="tableData"
-          stripe
-          style="width: 100%"
-          @cell-click="cellClickHandle"
-          :cell-style="packageDownload"
-        >
-          <el-table-column type="index" label="#" width="100px">
-          </el-table-column>
-          <el-table-column
-            prop="package"
-            label="包名"
-            width="550px"
-          ></el-table-column>
-          <el-table-column
-            prop="nickname"
-            label="上传人"
-            width="400px"
-          ></el-table-column>
-        </el-table>
-      </div>
+      <el-table
+        v-loading="loading"
+        element-loading-text="拼命加载中"
+        :data="tableData"
+        stripe
+        style="width: 100%"
+        @cell-click="cellClickHandle"
+        :cell-style="packageDownload"
+        v-if="this.queryStatus"
+      >
+        <el-table-column type="index" label="#" width="100px">
+        </el-table-column>
+        <el-table-column
+          prop="package"
+          label="包名"
+          width="550px"
+        ></el-table-column>
+        <el-table-column
+          prop="nickname"
+          label="上传人"
+          width="400px"
+        ></el-table-column>
+      </el-table>
       <el-empty
         description="未查询到相似包，欢迎登录补充上传哦~"
         v-else
@@ -60,16 +59,8 @@ export default {
       nickname: this.$router.currentRoute.value.query["nickname"],
       tableData: [],
       loading: true,
+      queryStatus: true,
     };
-  },
-  computed: {
-    queryStatus: function() {
-      if (this.tableData.length > 0) {
-        return true;
-      } else {
-        return false;
-      }
-    },
   },
   created() {
     if (this.package !== undefined) {
@@ -78,14 +69,22 @@ export default {
           // 查询包信息成功则展示
           this.tableData = res.data["data"];
           this.loading = false;
+        } else {
+          // 查询不到则给出提示
+          this.queryStatus = false;
         }
       });
     }
     if (this.username !== undefined) {
       personUploadDetail(this.username).then((res) => {
         if (res.data["message"] == "ok") {
+          // 查询包信息成功则展示
           this.tableData = res.data["data"];
           this.loading = false;
+          this.queryStatus = true;
+        } else {
+          // 查询不到则给出提示
+          this.queryStatus = false;
         }
       });
     }
